4fae4d08943b0de9398bac2124fd19bf73c65a87
Ravula Sonali <ravula.sonali@gmail.com>
2024-01-11 17:50:24 +0530

    UnitTestProject

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d2b140f-7683-4e64-8c46-d4ae44464d38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/TeacherTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SchoolOopLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class TeacherTest
+    {
+        [Test]
+        public void TestTeacherId()
+        {
+            var teacher = new Teacher();
+            teacher.TeacherId = 101;
+            Assert.AreEqual(101, teacher.TeacherId);
+        }
+        [Test]
+        public void TestTeacherName()
+        {
+            var teacher = new Teacher();
+            teacher.TeacherName = "Mrs.Sheela";
+            Assert.AreEqual("Mrs.Sheela", teacher.TeacherName);
+        }
+    }
+}
+
